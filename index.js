fcb49b747e08567cd720c1eaa9ba2e409a2c3fd6
Tom Butcher <thomasbutcher15@gmail.com>
2021-01-12 13:43:01 +0000

    Added PDF generation and blacklist plus small improvements

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
 const fs = require('fs');
 var path = require('path');
+const { mdToPdf } = require('md-to-pdf');
 
-const folders = ['/source/code/dir']
+const folders = ['/source/directory']
+const outputFileName = "output";
+const title = "Output Title";
+const blackList = [""];
+const makeMD = true;
+const makePDF = true;
 
-let textBuf = "# CodeDump \r\n";
+let textBuf = "# " + title + " \r\n";
 
+if (makeMD) {
+    console.log("Making Markdown file...")
+    folders.forEach(folder => {
+        const files = fs.readdirSync(folder, { withFileTypes: true });
+        const fileList = files.filter(files => files.isFile()).map(files => files.name);
+        fileList.forEach(file => {
+            if (file != ".DS_Store") {
+                if (!(blackList.indexOf(file) >= 0)) {
+                    textBuf += ("## " + file + "\r\n");
+                    const fileData = fs.readFileSync(folder + "/" + file);
+                    const fileExtention = path.extname(folder + "/" + file).replace(".", "");
+                    if (file.includes("CMake")) {
+                        textBuf += ("```cmake \r\n");
+                    } else {
+                        textBuf += ("```" + fileExtention + "\r\n");
+                    }
 
-folders.forEach(folder => {
-    const files = fs.readdirSync(folder, { withFileTypes: true });
-    const fileList = files.filter(files => files.isFile()).map(files => files.name);
-    fileList.forEach(file => {
-        if (file != ".DS_Store") {
-            textBuf += ("## " + file + "\r\n");
-            //console.log(folder + "/" + file);
-            const fileData = fs.readFileSync(folder + "/" + file);
-            const fileExtention = path.extname(folder + "/" + file).replace(".", "");
-            textBuf += ("```" + fileExtention + "\r\n");
-            textBuf += fileData;
-            textBuf += "```\r\n\r\n"
-        }
-
+                    textBuf += fileData;
+                    textBuf += "```\r\n\r\n"
+                }
+            }
+        });
     });
-});
 
-fs.writeFile("output.md", textBuf, "utf-8", function (err) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-});
+    fs.writeFile(outputFileName + ".md", textBuf, "utf-8", function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("MD file saved!");
+    });
+}
+
+if (!makeMD && !makePDF) {
+    console.log("ERROR! - You have disabled PDF and MD generation. Nothing will be made!")
+}
+
+if (!makeMD && makePDF) {
+    console.log("WARNING! - You have disabled MD generation but PDF generation is enabled. Please ensure you have a source markdown file!")
+}
+
+if (makePDF) {
+    
+        (async () => {
+            const sourceFileName = outputFileName + ".md"
+            console.log("Making PDF File...")
+            const pdf = await mdToPdf({ path: sourceFileName }).catch(console.error);
+
+            if (pdf) {
+                console.log("PDF file saved!")
+                fs.writeFileSync(outputFileName + ".pdf", pdf.content);
+            }
+        })();
+}
 //console.log(textBuf);
